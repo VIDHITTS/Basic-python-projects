fff0d39e49dd790b6422b56deb6ad55852714e95
VIDHITTS <vidhitts@gmail.com>
2025-01-22 23:36:00 +0530

    k

--- password_generator(robust).py
+++ password_generator(robust).py
@@ -5,6 +5,7 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&','*', '+']
 
 print("Welcome to the PyPassword Generator!")
+#user inputs
 user_letters = int(input("How many letters would you like in your password?\n"))
 user_symbols = int(input(f"How many symbols would you like?\n"))
 user_numbers = int(input(f"How many numbers would you like?\n"))
