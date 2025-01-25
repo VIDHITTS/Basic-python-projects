1389fff047f040d82bc7ecf0fd932a025d761720
VIDHITTS <vidhitts@gmail.com>
2025-01-25 21:42:41 +0530

    k

--- password_generator_basic(easy).py
+++ password_generator_basic(easy).py
@@ -9,6 +9,8 @@ user_letters = int(input("How many letters would you like in your password?\n"))
 user_symbols = int(input(f"How many symbols would you like?\n"))
 user_numbers = int(input(f"How many numbers would you like?\n"))
 
+
+
 password=""
 for _ in range(0,user_letters):
     random_character=random.choice(letters)
