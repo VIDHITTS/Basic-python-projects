f0c7f215ee1ae63cb82e931e13d5c624aa1ad7e4
VIDHITTS <vidhitts@gmail.com>
2025-01-30 22:40:05 +0530

    p

--- hangman.py
+++ hangman.py
@@ -88,6 +88,8 @@ print("Word to guess: " + placeholder)
 game_over = False
 correct_letters = []
 
+
+
 while not game_over:
 
     print(f"****************************{lives}/6 LIVES LEFT****************************")
